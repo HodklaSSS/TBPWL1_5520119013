574cc842d1de6486753e5684d6e4f3982a3359f2
Rifqi <panzerkampfwagentigerking@gmail.com>
2021-04-14 22:23:10 +0700

    migrate db + tambah adminlte + register isadmin kernel

---
+++ Laravel/TB/resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Dashboard</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-body">
+                   Hayuk
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
